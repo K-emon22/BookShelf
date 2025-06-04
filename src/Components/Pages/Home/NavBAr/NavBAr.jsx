700fa38cb1130d82608804546ce993eb8d64566a
K-emon22 <emonsheikhkhalid2@gmail.com>
2025-06-04 08:28:47 +0600

    navBar done

--- src/Components/Pages/Home/NavBAr/NavBAr.jsx
+++ src/Components/Pages/Home/NavBAr/NavBAr.jsx
@@ -1,57 +1,139 @@
 import React from "react";
 import {NavLink} from "react-router";
-
+import {HiMenuAlt3} from "react-icons/hi";
 const NavBAr = () => {
+  // const pages = (
+  //   <>
+  //     <NavLink
+  //       className={({isActive}) =>
+  //         ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg text-center" : ""}`
+  //       }
+  //       to={"/"}
+  //     >
+  //       Home
+  //     </NavLink>
+  //     <NavLink
+  //       className={({isActive}) =>
+  //         ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg text-center" : ""}`
+  //       }
+  //       to={"/bookshelf"}
+  //     >
+  //       Bookshelf
+  //     </NavLink>
+  //     <NavLink
+  //       className={({isActive}) =>
+  //         ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg text-center" : ""}`
+  //       }
+  //       to={"/addBook"}
+  //     >
+  //       Add Book
+  //     </NavLink>
+  //     <NavLink
+  //       className={({isActive}) =>
+  //         ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg text-center" : ""}`
+  //       }
+  //       to={"/myBooks"}
+  //     >
+  //       {" "}
+  //       My Books
+  //     </NavLink>
+  //     <NavLink
+  //       className={({isActive}) =>
+  //         ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg text-center" : ""}`
+  //       }
+  //       to={"/profile"}
+  //     >
+  //       Profile{" "}
+  //     </NavLink>
+  //   </>
+  // );
   const pages = (
     <>
       <NavLink
-        className={({isActive}) =>
-          ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg" : ""}`
-        }
         to={"/"}
+        className="relative px-2 py-1 font-semibold text-black overflow-hidden"
       >
-        Home
+        {({isActive}) => (
+          <>
+            <span
+              className={`absolute inset-0 bg-green-400 z-0 transition-transform duration-500 ease-in-out ${
+                isActive ? "scale-x-100" : "scale-x-0"
+              } origin-left rounded-lg`}
+            />
+            <span className="relative z-10">Home</span>
+          </>
+        )}
       </NavLink>
+
       <NavLink
-        className={({isActive}) =>
-          ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg" : ""}`
-        }
         to={"/bookshelf"}
+        className="relative px-2 py-1 font-semibold text-black overflow-hidden"
       >
-        Bookshelf
+        {({isActive}) => (
+          <>
+            <span
+              className={`absolute inset-0 bg-green-400 z-0 transition-transform duration-500 ease-in-out ${
+                isActive ? "scale-x-100" : "scale-x-0"
+              } origin-left rounded-lg`}
+            />
+            <span className="relative z-10">Bookshelf</span>
+          </>
+        )}
       </NavLink>
+
       <NavLink
-        className={({isActive}) =>
-          ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg" : ""}`
-        }
         to={"/addBook"}
+        className="relative px-2 py-1 font-semibold text-black overflow-hidden"
       >
-        Add Book
+        {({isActive}) => (
+          <>
+            <span
+              className={`absolute inset-0 bg-green-400 z-0 transition-transform duration-500 ease-in-out ${
+                isActive ? "scale-x-100" : "scale-x-0"
+              } origin-left rounded-lg`}
+            />
+            <span className="relative z-10">Add Book</span>
+          </>
+        )}
       </NavLink>
+
       <NavLink
-        className={({isActive}) =>
-          ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg" : ""}`
-        }
         to={"/myBooks"}
+        className="relative px-2 py-1 font-semibold text-black overflow-hidden"
       >
-        {" "}
-        My Books
+        {({isActive}) => (
+          <>
+            <span
+              className={`absolute inset-0 bg-green-400 z-0 transition-transform duration-500 ease-in-out ${
+                isActive ? "scale-x-100" : "scale-x-0"
+              } origin-left rounded-lg`}
+            />
+            <span className="relative z-10">My Books</span>
+          </>
+        )}
       </NavLink>
+
       <NavLink
-        className={({isActive}) =>
-          ` px-2 py-1 ${isActive ? " bg-green-400 rounded-lg" : ""}`
-        }
         to={"/profile"}
+        className="relative px-2 py-1 font-semibold text-black overflow-hidden"
       >
-        Profile{" "}
+        {({isActive}) => (
+          <>
+            <span
+              className={`absolute inset-0 bg-green-400 z-0 transition-transform duration-500 ease-in-out ${
+                isActive ? "scale-x-100" : "scale-x-0"
+              } origin-left rounded-lg`}
+            />
+            <span className="relative z-10">Profile</span>
+          </>
+        )}
       </NavLink>
     </>
   );
-
   return (
     <div className="sticky top-0 z-50 bg-transparent py-2">
       <nav className="flex flex-row justify-between mb-20 px-[2%]">
-        <div className="flex justify-between gap-2">
+        <div className="flex justify-between gap-2 ">
           <div className="flex justify-center items-center">
             <img
               className="h-10 w-15 rounded-lg"
@@ -59,57 +141,47 @@ const NavBAr = () => {
                 "https://i.ibb.co/Q7NsLYWn/572504ff-152a-44e5-9fb4-e4d0966bab63.jpg"
               }
               alt=""
-              
             />
           </div>
           <h1 className="my-auto font-bold text-2xl hidden lg:text-4xl [@media(min-width:851px)]:block">
             Bookshelf
           </h1>
+          
         </div>
-        <div className="my-auto hidden [@media(min-width:850Px)]:block">
-          {pages}
+        <div className="my-auto hidden [@media(min-width:850Px)]:block ">
+          <div className="flex gap-4  xl:gap-8 font-semibold">{pages}</div>
         </div>
         <div className="[@media(min-width:850px)]:hidden my-auto">
           <h1 className="my-auto font-bold text-2xl ">Bookshelf</h1>{" "}
         </div>
         <div className=" flex flex-row gap-5">
-          <div className="  hidden [@media(min-width:850Px)]:block">
+          <div className=" space-x-3 hidden [@media(min-width:850Px)]:block">
             <button className="btn btn-primary">Login </button>
             <button className="btn btn-primary"> Register</button>
           </div>
 
-
-
           <div className=" [@media(min-width:850px)]:hidden  my-auto ">
-
-
-
-
-<div className="dropdown dropdown-end">
-  <div tabIndex={0} role="button" className="btn m-1">Click  ⬇️</div>
-  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-<div className="flex flex-col">
-  {pages}
-</div>
-  </ul>
-</div>
-
-
-
+            <div className="dropdown dropdown-end ">
+              <div tabIndex={0} role="button" className=" m-1">
+                <HiMenuAlt3 size={25} className="mt-2.5" />
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm border-2"
+              >
+                <div className="flex flex-col font-semibold ">
+                  {pages}
+                  <hr className="mt-2" />
+                  <div className="flex flex-row justify-center items-center mt-5  gap-2">
+                    <button className="btn btn-primary">Login </button>
+                    <button className="btn btn-primary"> Register</button>
+                    
+                  </div>
+                </div>
+              </ul>
+            </div>
           </div>
-
-
-
-
-
-
-
-
         </div>
-
-
-
-
       </nav>
     </div>
   );
