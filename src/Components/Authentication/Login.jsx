a1d3287f4b8099f05c2ab560277d79a96317c7bf
K-emon22 <emonsheikhkhalid2@gmail.com>
2025-06-04 16:24:46 +0600

    login & register page design done

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,23 +1,51 @@
-import React from "react";
 import {Link} from "react-router";
-
+import {Typewriter} from "react-simple-typewriter";
+import {Fade} from "react-awesome-reveal";
+import Loder from "../Loder/Loder";
+import {useEffect, useState} from "react";
 const Login = () => {
+  const [loder, setLoder] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoder(false);
+    }, 500);
+  });
+
+  if (loder) {
+    return <Loder></Loder>;
+  }
+
   return (
     <div
       className="bg-cover bg-center h-screen px-[2%]"
-      style={{backgroundImage: `url('public/regiser.png')`}}
+      style={{backgroundImage: `url('https://i.ibb.co/7d9DHCrr/Sign-up-rafiki.png')`}}
     >
-      <h1 className="font-bold text-2xl sm:text-5xl text-center">
-        {" "}
-        Login & Let the Magic Begin ✨
-      </h1>
+      <Fade direction="down" cascade duration={800}>
+        <h1 className="text-3xl sm:text-5xl font-bold text-center">
+          Ready to{" "}
+          <span style={{color: "blue", fontWeight: "bold"}}>
+            <Typewriter
+              words={["Explore", "Create", "Learn", "Connect", "Inspire"]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </span>{" "}
+          <br className="sm:hidden" />
+          Login Now.....
+        </h1>
+      </Fade>
 
-      <div className="flex justify-center items-center ">
-        <form className="flex flex-col justify-center items-center min-h-[calc(100vh-100px)]  md:gap-2 w-2/5  ">
+      <div className="flex justify-center items-center min-h-[calc(100vh-100px)]">
+        <form className="flex flex-col justify-center items-center backdrop-blur-sm bg-blue-600/20 py-8 px-5 rounded-lg shadow shadow-black  md:gap-2  md:w-[400px]  ">
           <input
             type="text"
             name="email"
-            className="border-2 p-2 h-10 font-semibold rounded-lg md:w-full bg-blue-600/10"
+            className="border-2 border-blue-600 p-2 h-10 font-semibold rounded-lg md:w-full bg-white/40"
             placeholder=" Enter Your email"
           />
           <br />
@@ -25,24 +53,25 @@ const Login = () => {
           <input
             type="text"
             name="Password"
-            className="border-2 p-2 h-10 font-semibold rounded-lg md:w-full bg-blue-600/10"
+            className="border-2 border-blue-600 p-2 h-10 font-semibold rounded-lg md:w-full bg-white/40"
             placeholder=" Enter Your Password"
           />
 
-          <button className=" mt-8 font-bold border-2 w-[206.5px] md:w-full bg-[#F5F5F5]  h-10 my-auto rounded-full mb-2">
+          <button className=" hover:bg-blue-500 cursor-pointer mt-8 font-bold border-2 w-[206.5px] md:w-full bg-[#F5F5F5]   h-10 my-auto rounded-full mb-2">
             {" "}
             Google{" "}
           </button>
-          <button className=" bg-blue-500 text-white font-bold border-2 border-black w-[206.5px] md:w-full  h-10 rounded-full ">
+          <button className=" cursor-pointer bg-blue-500 text-white font-bold border-2 border-black w-[206.5px] md:w-full  h-10 rounded-full ">
             Register
           </button>
-          <h1>
+          <h1 className="text-center text-black">
             {" "}
-            Don’t have an account?{" "}
+            Don't have an account?
             <Link
               to={"/register"}
               className="text-blue-600 cursor-pointer font-semibold"
             >
+              {" "}
               Register Now
             </Link>
           </h1>
--- src/Components/Authentication/Register.jsx
+++ src/Components/Authentication/Register.jsx
@@ -1,19 +1,60 @@
-import React from "react";
 import {Link} from "react-router";
-
+import {Typewriter} from "react-simple-typewriter";
+import {Fade} from "react-awesome-reveal";
+import Loder from "../Loder/Loder";
+import {useEffect, useState} from "react";
 const Register = () => {
+  const [loder, setLoder] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoder(false);
+    }, 500);
+  });
+
+  if (loder) {
+    return <Loder></Loder>;
+  }
+
+  const register = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const photo = e.target.photo.value;
+    const password = e.target.password.value;
+    console.log(name, email, photo, password);
+  };
+
   return (
     <div
       className="bg-cover bg-center h-screen px-[2%]"
-      style={{backgroundImage: `url('public/Mobile login-rafiki.png')`}}
+      style={{
+        backgroundImage: `url('https://i.ibb.co/ZzKprfz6/Mobile-login-rafiki.png')`,
+      }}
     >
-      <h1 className="font-bold text-2xl sm:text-5xl text-center">
-        {" "}
-        Ready to Explore? Sign Up!
-      </h1>
+      <Fade direction="down" cascade duration={800}>
+        <h1 className="text-3xl sm:text-5xl font-bold text-center">
+          Ready to{" "}
+          <span style={{color: "blue", fontWeight: "bold"}}>
+            <Typewriter
+              words={["Explore", "Create", "Learn", "Connect", "Inspire"]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </span>{" "}
+          <br className="sm:hidden" /> Register Now.....
+        </h1>
+      </Fade>
 
-      <div className="flex justify-center items-center ">
-        <form className="flex flex-col justify-center items-center min-h-[calc(100vh-100px)]  md:gap-2 w-2/5  ">
+      <div className="flex justify-center items-center min-h-[calc(100vh-100px)]">
+        <form
+          onSubmit={register}
+          className="flex flex-col justify-center items-center backdrop-blur-sm bg-blue-600/20 py-5 px-5 rounded-lg shadow shadow-black  md:gap-2  md:w-[400px]  "
+        >
           <input
             type="text"
             name="name"
@@ -22,7 +63,7 @@ const Register = () => {
           />
           <br />
           <input
-            type="text"
+            type="email"
             name="email"
             className="border-2 border-blue-600 p-2 h-10 font-semibold rounded-lg md:w-full bg-white/40"
             placeholder=" Enter Your email"
@@ -30,26 +71,32 @@ const Register = () => {
           <br />
           <input
             type="text"
-            name="Photo"
+            name="photo"
             className="border-2 border-blue-800 p-2 h-10 font-semibold rounded-lg md:w-full bg-white/40"
             placeholder=" Enter Your Photo Url"
           />
           <br />
           <input
-            type="text"
-            name="Password"
+            type="password"
+            name="password"
             className="border-2 border-blue-600 p-2 h-10 font-semibold rounded-lg md:w-full bg-white/40"
             placeholder=" Enter Your Password"
           />
 
-          <button className=" mt-8 font-bold border-2 w-[206.5px] md:w-full bg-[#F5F5F5]   h-10 my-auto rounded-full mb-2">
+          <button
+            type="button"
+            className=" cursor-pointer mt-8 font-bold border-2 w-[206.5px] md:w-full bg-[#F5F5F5]   h-10 my-auto rounded-full mb-2"
+          >
             {" "}
             Google{" "}
           </button>
-          <button className=" bg-blue-500 text-white font-bold border-2 border-black w-[206.5px] md:w-full  h-10 rounded-full ">
+          <button
+            type="submit"
+            className=" cursor-pointer bg-blue-500 text-white font-bold border-2 border-black w-[206.5px] md:w-full  h-10 rounded-full "
+          >
             Register
           </button>
-          <h1>
+          <h1 className="text-center text-black">
             {" "}
             Already have an account?
             <Link
